dd6d6730a50284db3c40c97966ff82d5d846010a
isaac-enrique.cobos <isaac-enrique.cobos@soprasteria.com>
2019-12-02 10:36:49 +0100

    FEAT: components, Pages and routes working

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss']
+})
+export class InicioPage implements OnInit {
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirecTo: '/action-sheet'
+    },
+    {
+      icon: 'appstore',
+      name: 'alert',
+      redirecTo: '/alert'
+    }
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
+interface Componente {
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
